a9f2018880e65c6bcbf6f41633b7b5c2380b9e81
Luis Octávio <luisoctavius.sc@gmail.com>
2026-08-06 20:54:33 -0300

    primeiro commit com todos os exercícios concluídos até agora

---
+++ Simple API/Simple API/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Simple_API.DTOs;
+
+namespace Simple_API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductController
+    {
+        public static IList<Product> products = new List<Product>();
+        [HttpGet]
+        public Product? GetProduct(long id)
+        {
+            var product = products.FirstOrDefault(x => x?.Id == id, null);
+            if (product == null) return null;
+
+            return product;
+        }
+
+        public IList<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        [HttpPost]
+        public Product SaveProduct(Product product)
+        {
+            products.Add(product);
+            return product;
+        }
+
+        [HttpPatch]
+        public Product? UpdateProduct(Product product)
+        {
+            var existingProduct = products.FirstOrDefault(x => x?.Id == product.Id, null);
+
+            if (existingProduct == null) return null;
+
+            existingProduct.Price = product.Price;
+            existingProduct.Name = product.Name;
+
+            return existingProduct;
+        }
+
+        [HttpDelete]
+        public void DeleteProduct(long id)
+        {
+            var product = GetProductById(id);
+            if (product == null) return;
+
+            products.Remove(product);
+        }
+
+        private Product? GetProductById(long id)
+        {
+            Product product = products.FirstOrDefault(x => x?.Id == id, null);
+            if (product == null) return null;
+
+            return product;
+        }
+
+    }
+}
---
+++ Simple API/Simple API/DTOs/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Simple_API.DTOs
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Product(long id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+}
