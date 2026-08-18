a135c16d8e307f3e1c78b8415e209711c16896cf
Luis Octávio <luisoctavius.sc@gmail.com>
2026-08-17 22:26:04 -0300

    add odds game

---
+++ Guy_Odds/Guy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guys
+{
+    class Guy
+    {
+        public string Name;
+        public int Cash;
+
+        public Guy(string name, int cash)
+        {
+            Name = name;
+            Cash = cash;
+        }
+
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(Name + " has " + Cash + " bucks.");
+        }
+
+        public int GiveCash(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine(Name + " says: " + amount + " isn't a valid amount");
+                return 0;
+            }
+            if (amount > Cash)
+            {
+                Console.WriteLine(Name + " says: " + "I don't have enough money to give you " + amount);
+                return 0;
+            }
+            Cash -= amount;
+            return amount;
+        }
+
+        public void ReceiveCash(int amount)
+        {
+            if (amount <= 0 )
+            {
+                Console.WriteLine(Name + " says: " + amount + "isn't an amount I'll take");
+            }
+            else
+            {
+                Cash += amount;
+            }
+        }
+    }
+}
---
+++ Guy_Odds/Program.cs
@@ -0,0 +1,48 @@
+﻿using Guys;
+
+namespace Guy_Odds
+{
+    internal class Program
+    {
+        public static Random random = new Random();
+        public static double odds = 0.75;
+        public static Guy player = new Guy("The player", 100);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the cassino. The odds are ",  odds);
+
+            while (player.Cash > 0)
+            {
+                player.WriteMyInfo();
+
+                Console.Write("How much do you want to bet in the little tiger?? ");
+
+                string? howMuch = Console.ReadLine();
+
+                if (int.TryParse(howMuch, out int amount))
+                {
+                    int pot = amount * 2;
+                    int randomNum = random.Next(1);
+
+                    if (pot > player.Cash)
+                    {
+                        break;
+                    }
+
+                    if (randomNum > odds)
+                    {
+                        player.ReceiveCash(pot);
+                        Console.WriteLine("You win ", pot);
+                    }
+                    else
+                    {
+                        player.GiveCash(pot);
+                        Console.WriteLine($"You lose {amount} bucks.");
+                        Console.WriteLine("The house always wins, loser.");
+                    }
+                }
+            }
+
+        }
+    }
+}
